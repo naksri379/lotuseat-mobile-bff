6e2eee7ca8f2a71f961d71900dbdda9856ee82b6
nawachat.mua <nm@manaosoftware.com>
2021-12-16 16:41:30 +0700

    add service getToken
    add service getToken

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,12 +1,11 @@
 import { HttpService, Injectable } from '@nestjs/common'
 import { ExecuteTimeLog } from '../../middleware/decorator/executeTimeLog.decorator'
-
-//import { CategoryServiceHelper } from './category.service.helper'
 import { GetCategoryListRequestDto } from './models/category.request'
 import { GetCategoryListResponseDto } from './models/category.response'
 import { mockCategoryListRawData } from '../../tests/mocks/category.service.mock'
 import { CategoryServiceHelper } from './category.service.helper'
-
+import { weOmniAuth } from 'src/utilities/weOmniAuth';
+const qs = require('querystring');
 @Injectable()
 export class CategoryService {
   constructor(
@@ -29,9 +28,32 @@ export class CategoryService {
       .toPromise()
 
     if (payload.status === 200) {
-      return this.categoryServiceHelper.mapCategoryListResponse(
-        mockCategoryListRawData
-      )
+       return this.categoryServiceHelper.mapCategoryListResponse(
+        mockCategoryListRawData)      
     }
   }
-}
+
+   async getToken() {
+    const requestBody = {
+      grant_type: 'client_credentials',
+    }    
+    const config = {
+      headers: {
+             'Cookie' : 'AWSALB=Db5313RTMK4TNMhTKQLtSbcr7uG9bZ0NasJXs4XJiUHzzjjKQpYKYfsvTCdREOVokoi1DFYOIp8bZq+Xy0fEJ2I6ZunGgZPnYiVPH5RCJ3QKkr1+3ljQZjhue4Hh; AWSALBCORS=Db5313RTMK4TNMhTKQLtSbcr7uG9bZ0NasJXs4XJiUHzzjjKQpYKYfsvTCdREOVokoi1DFYOIp8bZq+Xy0fEJ2I6ZunGgZPnYiVPH5RCJ3QKkr1+3ljQZjhue4Hh; XSRF-TOKEN=c111118f-5f77-42b3-a50d-3cdfd81904d2',
+        'Content-Type': 'application/x-www-form-urlencoded', 
+        'Authorization': `Basic ${process.env.ENCOED_TOKEN}`,
+        'Accept-Encoding': 'gzip, deflate, br',
+      }
+    }
+    let url = 'https://platform.weomni.com/uaa/oauth/token'
+    const payload = await this.httpService
+    .post(url, qs.stringify(requestBody), config)
+    .toPromise()
+     
+    console.log('res',payload.data.access_token)
+    return payload.data.access_token
+   }
+    
+ 
+  }
+
